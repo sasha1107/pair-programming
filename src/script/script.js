854a010cff17ff3964cd75ee92d07dc4410d5d6f
2DC <kkts9308@gmail.com>
2022-10-24 17:46:22 +0900

    김태수 자금 관련 세팅 중

--- src/script/script.js
+++ src/script/script.js
@@ -1,8 +1,22 @@
 let 입금액 = 0;
-let 소지금액 = 0;
+let 소지금액 = 25000;
 let 잔액 = 0;
 let 총금액 = 0;
 
 const 입금액입력 = document.querySelector('.Deposit');
 const 입금버튼 = document.querySelector('.Deposit_btn');
 const 잔액디스플레이 = document.querySelector('.won');
+const 소지금디스플레이 = document.querySelector('.fund_won');
+
+소지금디스플레이.textContent = `${소지금액.toLocaleString()}원`
+
+function 자금셋팅() {
+    소지금디스플레이.textContent = `${소지금액.toLocaleString()}원`
+}
+
+입금버튼.addEventListener('click', () => {
+    let 입금액 = +입금액입력.value
+    잔액디스플레이.textContent = `${입금액.toLocaleString()}원`
+    // 소지금액 - 입금액
+    자금셋팅()
+})
