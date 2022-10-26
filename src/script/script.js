cc8497b5d4cdc0e90c35ee18d8564d4235c648d0
Soohyun Jung <sasha1107@naver.com>
2022-10-26 18:06:30 +0900

    Feat: 거스름돈 반환 기능 추가

--- src/script/script.js
+++ src/script/script.js
@@ -1,22 +1,37 @@
 let 입금액 = 0;
 let 소지금액 = 25000;
 let 잔액 = 0;
-let 총금액 = 0;
+let 총금액 = 0; // 선택한 음료 * 가격의 합
 
 const 입금액입력 = document.querySelector('.Deposit');
 const 입금버튼 = document.querySelector('.Deposit_btn');
 const 잔액디스플레이 = document.querySelector('.won');
 const 소지금디스플레이 = document.querySelector('.fund_won');
-
-소지금디스플레이.textContent = `${소지금액.toLocaleString()}원`
+const 거스름돈버튼 = document.querySelector('.Change_btn');
 
 function 자금셋팅() {
-    소지금디스플레이.textContent = `${소지금액.toLocaleString()}원`
+    소지금디스플레이.textContent = `${소지금액.toLocaleString()}원`;
+    잔액디스플레이.textContent = `${잔액.toLocaleString()}원`;
 }
 
+자금셋팅();
 입금버튼.addEventListener('click', () => {
+    if (+입금액입력.value > 소지금액){
+        alert("소지금 부족!");
+        return;
+    }
     let 입금액 = +입금액입력.value
-    잔액디스플레이.textContent = `${입금액.toLocaleString()}원`
-    // 소지금액 - 입금액
-    자금셋팅()
+    잔액 += 입금액;
+    소지금액 -= 입금액;
+    자금셋팅();
+})
+
+거스름돈버튼.addEventListener('click', ()=>{
+    if (잔액 === 0){
+        alert("거스름돈없어!");
+        return;
+    }
+    소지금액+=잔액;
+    잔액 = 0;
+    자금셋팅();
 })
